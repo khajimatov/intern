7094cbd8adcc058ed49673206cab1def8566a090
khajimatov <for.official.bkh@gmail.com>
2022-09-30 10:46:07 +0600

    Added functionality to the Delete Button

--- XMLHttpRequest/main.js
+++ XMLHttpRequest/main.js
@@ -1,7 +1,16 @@
-// document.addEventListener('click', (e) => {
-//     if (e.target.className === "buttonDelete") {
-
-//     }
-// })
-// a = document.getElementsByClassName('container').forEach((b) => { console.log(b) })
-// console.log(a)
+document.addEventListener('click', (e) => {
+    switch (e.target.className) {
+        case "renderItems":
+            renderItems();
+            break;
+        case "buttonDelete":
+            deleteItem(e.target.parentNode.parentNode.parentNode.id)
+            break;
+        case "buttonEdit":
+            console.log(e.target.parentNode.parentNode.parentNode.id)
+            break;
+        case "buttonAdd":
+            console.log(e.target.parentNode.parentNode.parentNode.id)
+            break;
+    }
+})
