ed521f41ade165f45e340a0af3c40842f566d894
khajimatov <for.official.bkh@gmail.com>
2022-09-30 10:14:10 +0600

    Separated js from html file to main.js

---
+++ XMLHttpRequest/main.js
@@ -0,0 +1,7 @@
+// document.addEventListener('click', (e) => {
+//     if (e.target.className === "buttonDelete") {
+
+//     }
+// })
+// a = document.getElementsByClassName('container').forEach((b) => { console.log(b) })
+// console.log(a)
