9d81811e214650d5110fba905b545f407f474004
khajimatov <for.official.bkh@gmail.com>
2022-10-03 18:27:26 +0600

    Added Drag n Drop

--- todo-app/main.js
+++ todo-app/main.js
@@ -3,18 +3,19 @@ window.onload = function () {
     const DOING = "doing";
     const DONE = "done";
 
-    !localStorage.getItem(TODO) && localStorage.setItem(TODO, JSON.stringify([]))
-    !localStorage.getItem(DOING) && localStorage.setItem(DOING, JSON.stringify([]))
-    !localStorage.getItem(DONE) && localStorage.setItem(DONE, JSON.stringify([]))
+    !localStorage.getItem(TODO) && localStorage.setItem(TODO, JSON.stringify([]));
+    !localStorage.getItem(DOING) && localStorage.setItem(DOING, JSON.stringify([]));
+    !localStorage.getItem(DONE) && localStorage.setItem(DONE, JSON.stringify([]));
 
-    let todoList = JSON.parse(localStorage.todo)
-    let doingList = JSON.parse(localStorage.doing)
-    let doneList = JSON.parse(localStorage.done)
+    let todoList = JSON.parse(localStorage.todo);
+    let doingList = JSON.parse(localStorage.doing);
+    let doneList = JSON.parse(localStorage.done);
 
     function renderCards() {
-        let todoList = JSON.parse(localStorage.todo)
-        let doingList = JSON.parse(localStorage.doing)
-        let doneList = JSON.parse(localStorage.done)
+
+        let todoList = JSON.parse(localStorage.todo);
+        let doingList = JSON.parse(localStorage.doing);
+        let doneList = JSON.parse(localStorage.done);
 
         const todoCards = document.getElementById("todo");
         const doingCards = document.getElementById("doing");
@@ -25,9 +26,10 @@ window.onload = function () {
         doneCards.textContent = "";
 
         function listCards(id, section, className, text) {
+
             let newCard = document.createElement('div');
             let newCardText = document.createElement('p');
-            newCard.setAttribute('class', 'card ' + className);
+            newCard.classList.add('card', className);
             newCard.setAttribute('id', id);
             newCard.setAttribute('draggable', 'true');
             newCardText.setAttribute('class', 'cardText');
@@ -35,7 +37,8 @@ window.onload = function () {
 
             newCard.appendChild(newCardText);
             let column = document.querySelector('#' + section);
-            column && column.appendChild(newCard)
+            column.appendChild(newCard);
+
         }
 
         todoList.map((elm) => {
@@ -49,23 +52,28 @@ window.onload = function () {
         doneList.map((elm) => {
             listCards(elm.id, elm.section, elm.className, elm.text);
         })
+
+        const card = document.querySelectorAll('.card');
+        card.forEach(elm => {
+            elm.addEventListener('dragstart', (e) => {
+                e.dataTransfer.setData('text', e.target.id);
+                console.log('Есть триггер захвата' + e.dataTransfer);
+            })
+        })
+
+
     }
 
     renderCards();
 
     const zone = document.querySelectorAll('.task-column');
-    const card = document.querySelectorAll('.card');
 
     zone.forEach(elm => {
         elm.addEventListener('dragover', (e) => {
             e.preventDefault();
         })
-    })
-
-    card.forEach(elm => {
-        elm.addEventListener('dragstart', (e) => {
-            e.dataTransfer.setData('text', e.target.id);
-            console.log(e.dataTransfer)
+        elm.addEventListener('dragleave', (e) => {
+            e.preventDefault();
         })
     })
 
@@ -73,55 +81,71 @@ window.onload = function () {
     zone.forEach(elm => {
         elm.addEventListener('drop', (e) => {
             e.preventDefault();
+
             let elmId = e.dataTransfer.getData('text');
-            e.dataTransfer.clearData();
             let transferingCard = document.getElementById(elmId);
-            console.log(transferingCard)
-            console.log(elmId)
+
             let previosSection = transferingCard.parentNode.id;
-            let section = elm.querySelector('.cards').id;
-            let newClassName = section === 'doing' ? 'doingCard' : 'doneCard';
-            transferingCard.setAttribute('class', 'card ' + newClassName);
-            let newItem = { "id": elmId, "section": section, "className": newClassName, "text": transferingCard.textContent }
 
-            if (section === 'doing') {
-                JSON.stringify(newItem);
-                doingList.push(newItem);
-                localStorage.setItem(DOING, JSON.stringify(doingList));
+            let nextSection = elm.querySelector('.cards').id;
+
+            switch (nextSection) {
+                case "doing":
+                    newClassName = "doingCard";
+                    break;
+                case "done":
+                    newClassName = "doneCard";
+                    break;
+                case "todo":
+                    newClassName = "todoCard";
+                    break;
+                case previosSection:
+                    newClassName = previosSection + "Card";
+                    break;
+            }
+
+            transferingCard.classList.add('card', newClassName);
+
+            let newItem = { "id": elmId, "section": nextSection, "className": newClassName, "text": transferingCard.textContent }
+
+            if (nextSection !== previosSection) {
+                if (nextSection === "doing") {
+                    JSON.stringify(newItem);
+                    doingList.push(newItem);
+                    localStorage.setItem(DOING, JSON.stringify(doingList));
+                }
+                else if (nextSection === "done") {
+                    JSON.stringify(newItem);
+                    doneList.push(newItem);
+                    localStorage.setItem(DONE, JSON.stringify(doneList));
+                }
+                else if (nextSection === "todo") {
+                    JSON.stringify(newItem);
+                    todoList.push(newItem);
+                    localStorage.setItem(TODO, JSON.stringify(todoList));
+                }
+                if (previosSection === 'todo') {
+                    const todoIndex = todoList.findIndex((elm) => elm.id.toString() === elmId);
+                    if (todoIndex > -1) {
+                        todoList.splice(todoIndex, 1);
+                        localStorage.setItem(TODO, JSON.stringify(todoList));
+                    }
+                } else if (previosSection === 'doing') {
+                    const doingIndex = doingList.findIndex((elm) => elm.id.toString() === elmId);
+                    if (doingIndex > -1) {
+                        doingList.splice(doingIndex, 1);
+                        localStorage.setItem(DOING, JSON.stringify(doingList));
+                    }
+                } else if (previosSection === 'done') {
+                    const doneIndex = doneList.findIndex((elm) => elm.id.toString() === elmId);
+                    if (doneIndex > -1) {
+                        doneList.splice(doneIndex, 1);
+                        localStorage.setItem(DONE, JSON.stringify(doneList));
+                    }
+                }
+                elm.querySelector('.cards').appendChild(transferingCard)
                 renderCards();
             }
-            else {
-                JSON.stringify(newItem);
-                doneList.push(newItem);
-                localStorage.setItem(DONE, JSON.stringify(doneList));
-                renderCards();
-            }
-
-            elm.querySelector('.cards').appendChild(transferingCard)
-
-            if (previosSection === 'todo' && section !== 'todo') {
-                const todoIndex = todoList.findIndex((elm) => elm.id.toString() === elmId);
-                if (todoIndex > -1) {
-                    todoList.splice(todoIndex, 1);
-                    localStorage.setItem('todo', JSON.stringify(todoList));
-                    renderCards();
-                }
-            } else if (previosSection === 'doing' && section !== 'doing') {
-                const doingIndex = doingList.findIndex((elm) => elm.id.toString() === elmId);
-                if (doingIndex > -1) {
-                    doingList.splice(doingIndex, 1);
-                    localStorage.setItem('doing', JSON.stringify(doingList));
-                    renderCards();
-                }
-            } else if (previosSection === 'done' && section !== 'done') {
-                const doneIndex = doneList.findIndex((elm) => elm.id.toString() === elmId);
-                if (doneIndex > -1) {
-                    doneList.splice(doneIndex, 1);
-                    localStorage.setItem('done', JSON.stringify(doneList));
-                    renderCards();
-                }
-            }
-            addDragToCard()
         })
     })
 
@@ -133,12 +157,12 @@ window.onload = function () {
         else {
             document.querySelector('.warning').style.display = "none";
             let todoList = JSON.parse(localStorage.todo);
-            let id = Math.floor(Math.random() * 1000);
+            let id = Math.floor(Math.random() * 10000);
             let newCard = { "id": id, "section": "todo", "className": "todoCard", "text": document.getElementById('addCardText').value };
             JSON.stringify(newCard);
             todoList.push(newCard);
             localStorage.setItem('todo', JSON.stringify(todoList));
+            renderCards();
         }
-        renderCards();
     })
 }
